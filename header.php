60e049b77747cbe92b9584a26db20d3fa5e7f93b
zkohlmeier <84145902+zkohlmeier@users.noreply.github.com>
2023-05-19 12:29:07 -0400

    Fixes for Search & HR People Expertise

--- header.php
+++ header.php
@@ -47,8 +47,7 @@
 
 		<?php do_action( 'after_body_open' ); ?>
 
-
-		<?php if ( $ucfwp_header_markup = ucfwp_get_header_markup() && !is_search() ) : ?>
+		<?php if ( $ucfwp_header_markup = ucfwp_get_header_markup() ) : ?>
 		<header class="site-header">
 			<?php echo $ucfwp_header_markup; ?>
 		</header>
