53db529f99d28e2d4a5e9a28a57bdf0f85ef3cb0
Mike Setzer <michaelsetzerucf@gmail.com>
2023-06-13 16:25:47 -0400

    Revert "Update search.php"
    This reverts commit 08bce90b3763fd22ef7f38c51dcc100bf04faf2b.

--- search.php
+++ search.php
@@ -1,5 +1,8 @@
 <?php /*require_once get_template_directory() . '/index.php'; */?>
 
+<?php get_header(); ?>
+
+
 <!-- Hero image and title -->
 <div class="hero container-fluid">
 
@@ -9,7 +12,7 @@
         <div class="headerText">
 
             <h1>"<em><?php the_search_query(); ?></em>"</h1><br/>
-
+            
             <h2>Search Results</h2>
 
         </div>
@@ -23,13 +26,13 @@
 
                     <?php while ( have_posts() ) {
 
-                        the_post();
+                        the_post(); 
 
-                            $id = get_the_id();
+                            $id = get_the_id(); 
                             $pod = pods( 'forms_and_documents', $id );
                             $url = $pod->field( 'location_url' );
-                            $link = get_permalink();
-
+                            $link = get_permalink(); 
+                            
                             if ($url != ''): ?>
                                 <a href="<?php echo $url; ?>" target="blank" class="result">
                             <?php else: ?>
@@ -40,7 +43,7 @@
                                 </a>
 
                     <?php  } ?>
-
+                
                         <div class="pagination">
                             <div class="nav-previous"><?php previous_posts_link( 'Previous Page' ); ?></div>
                             <div class="nav-next"><?php next_posts_link( 'Next Page' ); ?></div>
@@ -52,13 +55,13 @@
     </div>
 
 <?php } else { ?>
-
-
+    
+    
     <div class="container">
         <div class="headerText">
 
             <h1>"<em><?php the_search_query(); ?></em>"</h1><br/>
-
+            
             <h2>No Results</h2>
 
         </div>
