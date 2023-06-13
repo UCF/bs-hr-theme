29fd079fb2b018682991ce4d8505caaec49c8f4e
Mike Setzer <michaelsetzerucf@gmail.com>
2023-06-13 17:02:05 -0400

    Update search.php

--- search.php
+++ search.php
@@ -6,82 +6,81 @@
 <!-- Hero image and title -->
 <div class="hero container-fluid">
 
-<?php if ( have_posts() ) { ?>
+	<?php if ( have_posts() ) { ?>
 
-    <div class="container">
-        <div class="headerText">
+	<div class="container">
+		<div class="headerText">
 
-            <h1>"<em><?php the_search_query(); ?></em>"</h1><br/>
+			<h1>"<em><?php the_search_query(); ?></em>"</h1><br/>
 
-            <h2>Search Results</h2>
+			<h2>Search Results</h2>
 
-        </div>
-    </div>
+		</div>
+	</div>
 </div>
 
-    <div class="container-fluid content-container white">
-        <div class="container">
-            <div class="row">
-                <div class="col col-lg-12 ">
+	<div class="container-fluid content-container white">
+		<div class="container">
+			<div class="row">
+				<div class="col col-lg-12 ">
 
-                    <?php while ( have_posts() ) {
+					<?php while ( have_posts() ) {
 
-                        the_post();
+					the_post();
 
-                            $id = get_the_id();
-                            $pod = pods( 'forms_and_documents', $id );
-                            $url = $pod->field( 'location_url' );
-                            $link = get_permalink();
+					$id = get_the_id();
+					$pod = pods( 'forms_and_documents', $id );
+					$url = $pod->field( 'location_url' );
+					$link = get_permalink();
 
-                            if ($url != ''): ?>
-                                <a href="<?php echo $url; ?>" target="blank" class="result">
-                            <?php else: ?>
-                                <a href="<?php echo get_post_permalink() ?>" class="result">
-                            <?php endif; ?>
-                                    <h3><?php the_title(); ?></h3>
-                                    <p><?php the_excerpt(); ?></p>
-                                </a>
+					if ($url != ''): ?>
+					<a href="<?php echo $url; ?>" target="blank" class="result">
+						<?php else: ?>
+						<a href="<?php echo get_post_permalink() ?>" class="result">
+							<?php endif; ?>
+							<h3><?php the_title(); ?></h3>
+							<p><?php the_excerpt(); ?></p>
+						</a>
 
-                    <?php  } ?>
+						<?php  } ?>
 
-                        <div class="pagination">
-                            <div class="nav-previous"><?php previous_posts_link( 'Previous Page' ); ?></div>
-                            <div class="nav-next"><?php next_posts_link( 'Next Page' ); ?></div>
-                        </div>
+						<div class="pagination">
+							<div class="nav-previous"><?php previous_posts_link( 'Previous Page' ); ?></div>
+							<div class="nav-next"><?php next_posts_link( 'Next Page' ); ?></div>
+						</div>
 
-                </div>
-            </div>
-        </div>
-    </div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 <?php } else { ?>
 
 
-    <div class="container">
-        <div class="headerText">
+	<div class="container">
+		<div class="headerText">
 
-            <h1>"<em><?php the_search_query(); ?></em>"</h1><br/>
+			<h1>"<em><?php the_search_query(); ?></em>"</h1><br/>
 
-            <h2>No Results</h2>
+			<h2>No Results</h2>
 
-        </div>
-    </div>
-</div>
+		</div>
+	</div>
+	</div>
 
-    <div class="container-fluid content-container white">
-        <div class="container">
-            <div class="row">
-                <div class="col col-lg-12 ">
+	<div class="container-fluid content-container white">
+		<div class="container">
+			<div class="row">
+				<div class="col col-lg-12 ">
 
-                    <p class="no-results">No Results</p>
+					<p class="no-results">No Results</p>
 
-                </div>
-            </div>
-        </div>
-    </div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 
 <?php } ?>
 
 <?php get_footer(); ?>
-
