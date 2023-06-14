fe409ba858ac209c41886962ee279b9540027130
Mike Setzer <michaelsetzerucf@gmail.com>
2023-06-14 10:59:56 -0400

    Update search.php

--- search.php
+++ search.php
@@ -10,7 +10,7 @@
 			<div class="header-content-inner align-self-start pt-4 pt-sm-0 align-self-sm-center">
 				<div class="container">
 					<div class="d-inline-block bg-primary-t-1">
-						<h1 class="header-title">"<?php the_search_query(); ?>"</h1><br/>
+						<h1 class="header-title">"<?php the_search_query(); ?>"</h1>
 					</div>
 					<div class="clearfix"></div>
 					<div class="d-inline-block bg-inverse">
